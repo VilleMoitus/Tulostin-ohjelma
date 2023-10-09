e4dfc5834bda18da8c906f9cb20aa090576449a3
Ville Moitus <ville.moitus@student.hamk.fi>
2023-10-09 22:30:19 +0300

    lisättiin lasku toimituksia ja desimaali
    lisättiin desimaalit jotta jakolaskun voi tehdä

--- src/TulostinOhjelma.java
+++ src/TulostinOhjelma.java
@@ -12,17 +12,22 @@ public class TulostinOhjelma {
 		System.out.println(tekija);
 		
 		//määritettään ja tulostetaan luku1
-		int luku1 = 5;
+		double luku1 = 5.0;
 		System.out.println("Luku1-muuttujan arvo on " + luku1);
 		
 		//määritetään ja tulostetaan luku2 
-		int luku2 = 2;
+		double luku2 = 2.0;
 		 System.out.println("Luku2-muuttujan arvo on " + luku2);
 		
 		//tehdään kolmas muuttuja tulo
-		int tulo;
+		 double tulo;
 		tulo = luku1 *luku2;
 		System.out.println(luku1 + " * " + luku2 + " = " + tulo);
+		
+		//laskutoimitukset 
+		double erotus = luku1 - luku2;
+		double summa = luku1 + luku2;
+        double jako = (double) luku1 / luku2;
 		
 		
 	}
