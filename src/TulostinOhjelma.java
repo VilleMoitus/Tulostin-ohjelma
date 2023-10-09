01d05d421f3dc1b0938e4af4e55ebb2cc5771f0b
Ville Moitus <ville.moitus@student.hamk.fi>
2023-10-09 22:11:38 +0300

    tehtiin ohjelman tekijän tulostus loppuun

--- src/TulostinOhjelma.java
+++ src/TulostinOhjelma.java
@@ -2,12 +2,13 @@
 public class TulostinOhjelma {
 
 	public static void main(String[] args) {
-		// vaihe 2. tulostetaan "hei olen tulostin ohjelma"
+		// tulostetaan "hei olen tulostin ohjelma"
 		System.out.println("Hei olen Tulostin-ohjelma");
 		
-		// vaihe 3. "tulostetaan ohjelman tekijä:"
+		// tulostetaan ohjelman tekijä
 		String tekija ;
 		tekija = "Ville Moitus";
+		System.out.print("Ohjelman tekijä: ");
 		System.out.println(tekija);
 		
 		
