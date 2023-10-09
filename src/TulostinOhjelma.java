bee7455e7ce1dc9ed1a97f4d6ae4040b8f89fe4c
Ville Moitus <ville.moitus@student.hamk.fi>
2023-10-09 21:57:18 +0300

    ensimmäinen tulostus ja ohjelman tekijä
    tehtiin tehtävän annon vaiheet 1-3.

--- src/TulostinOhjelma.java
+++ src/TulostinOhjelma.java
@@ -2,8 +2,15 @@
 public class TulostinOhjelma {
 
 	public static void main(String[] args) {
+		// vaihe 2. tulostetaan "hei olen tulostin ohjelma"
 		System.out.println("Hei olen Tulostin-ohjelma");
-
+		
+		// vaihe 3. "tulostetaan ohjelman tekijä:"
+		String tekija ;
+		tekija = "Ville Moitus";
+		System.out.println(tekija);
+		
+		
 	}
 
 }
